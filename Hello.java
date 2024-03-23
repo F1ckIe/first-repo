0d92906caaef1d5f093a1ccdfa261b275189e31e
fickle <3508166507@qq.com>
2024-03-23 13:54:20 +0800

    第一次提交

---
+++ Hello.java
@@ -0,0 +1,16 @@
+//2024.3.23
+//by cjm
+
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("你好,很高兴学习Java");
+        A a = new A();
+        a.fA();
+    }
+}
+
+class A {
+    void fA() {
+        System.out.println("We are students");
+    }
+}
