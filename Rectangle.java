c735117ba607b73d9bfed5dcd89571b3002e5368
fickle <3508166507@qq.com>
2024-03-27 20:31:37 +0800

    vscode上进行提交

--- Rectangle.java
+++ Rectangle.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Rectangle {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
         System.out.println("please input wide:");
-        Double width = read.nextDouble();
+        Double width = reader.nextDouble();
         System.out.println("please input height:");
-        Double height = read.nextDouble();
+        Double height = reader.nextDouble();
         Double area = width * height;
         System.out.printf("%10s%-10.2f", "wide:", width);
         System.out.printf("%4s%-4.2f", "height:", height);
